cddd01d7688e5fb914ab58fbf3d0da2ce9ba7d68
Gushenp <gushenriqp861programer@gmail.com>
2026-06-05 22:12:25 -0300

    Correção calculo de resultado

--- scripts/interface.gd
+++ scripts/interface.gd
@@ -3,37 +3,40 @@ extends Control
 @onready var campoDeEntrada = $LineEdit
 
 func _ready() -> void:
+	cacularResultadoEsperado()
 	pass
 	
-func _on_button_pressed() -> void:
-	cacularResultadoEsperado()
-			
-#func calcularAcerto():
-#	var entradaUsuario = campoDeEntrada.text
-#	
-#	if entradaUsuario.is_valid_int():
-#		var numero = entradaUsuario.to_int()
+func _process(delta: float) -> void:
+	#enterPressionado()
+	calcularAcerto()
 
-func retornarQuantidadeCasaMax(campo):
-	if campo == 1:
+	
+func enterPressionado():
+	if Input.is_action_just_pressed("enter"):
+		calcularAcerto()
+
+
+func retornarQuantidadeCasaMax(value):
+	if value == 1:
 		return 9
 		
-	if campo == 2:
+	if value == 2:
 		return 99
 		
-	if campo == 3: 
+	if value == 3: 
 		return 999
 		
-func retornarQuantidadeCasaMin(campo):
-	if campo == 1:
+func retornarQuantidadeCasaMin(value):
+	if value == 1:
 		return 1
 		
-	if campo == 2:
+	if value == 2:
 		return 10
 		
-	if campo == 3: 
+	if value == 3: 
 		return 100
 	
+var resultado
 func cacularResultadoEsperado():
 	var numeroMax1 = retornarQuantidadeCasaMax(Dados.campo1ID)
 	var numeroMax2 = retornarQuantidadeCasaMax(Dados.campo2ID)
@@ -43,5 +46,20 @@ func cacularResultadoEsperado():
 	var numero1 = randi_range(numeroMin1, numeroMax1)
 	var numero2 = randi_range(numeroMin2, numeroMax2)
 	
-	print(numero1)
-	print(numero2)
+	if (Dados.operacaoID == 0):
+		resultado = numero1 + numero2
+		
+	print(numero1, "+", numero2, "=", resultado)
+
+func calcularAcerto():
+	var entradaUsuario = campoDeEntrada.text
+	
+	if entradaUsuario.is_valid_int():
+		var numeroUsuario = entradaUsuario.to_int()
+		
+		if numeroUsuario == resultado:
+			print("Acertou!")
+			campoDeEntrada.clear()
+			cacularResultadoEsperado()
+		
+		campoDeEntrada.grab_focus()
