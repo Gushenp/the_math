9f760657cf8d3858371a465de258d8c38b34a5a0
Gushenp <gushenriqp861programer@gmail.com>
2026-06-06 23:08:20 -0300

    Adição de áudio simples

--- scripts/interface.gd
+++ scripts/interface.gd
@@ -4,8 +4,11 @@ extends Control
 @onready var campo1View = $Campo1
 @onready var campo2View = $Campo2
 @onready var OperacaoView = $Operacao
+@onready var audioTecla = $key
+@onready var correctAudio = $correct
 
 func _ready() -> void:
+	campoDeEntrada.grab_focus()
 	cacularResultadoEsperado()
 	pass
 	
@@ -34,6 +37,7 @@ func retornarQuantidadeCasaMin(value):
 		return 100
 	
 var resultado
+var contador
 func cacularResultadoEsperado():
 	var numeroMax1 = retornarQuantidadeCasaMax(Dados.campo1ID)
 	var numeroMax2 = retornarQuantidadeCasaMax(Dados.campo2ID)
@@ -60,12 +64,22 @@ func calcularAcerto():
 	if entradaUsuario.is_valid_int():
 		var numeroUsuario = entradaUsuario.to_int()
 		
-		if numeroUsuario == resultado:
+		if numeroUsuario == resultado and contador != 10:
 			print("Acertou!")
+			correctAudio.play()
 			campoDeEntrada.clear()
 			cacularResultadoEsperado()
 		
+		
 		campoDeEntrada.grab_focus()
+		
+func _input(event):
+	if event is InputEventKey and event.is_pressed() and not event.is_echo():
+		tocarSomTecla()
+
+func tocarSomTecla():
+	audioTecla.pitch_scale = randf_range(0.9, 1.3)
+	audioTecla.play()
 
 func atualizarVisual(numero1, numero2):
 	campo1View.text = str(numero1)
